601b5ac96ffa03cf1fdc05173af1b0aacb7b0d7d
karyna.ro <karyna.ro@onyx.com>
2022-04-02 20:12:16 +0300

    entity product creation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +16,27 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('main/default/index.html.twig');
+        $entityManager = $this->getDoctrine()->getManager();
+        $prodList=$entityManager->getRepository(Product::class)->findAll();
+        dd($prodList);
+        return $this->render('main/default/index.html.twig',[]);
     }
+
+    /**
+     * @Route("/product-add", name="product_add")
+     */
+        public function productAdd(Request $request) :Response
+        {
+            $product = new Product();
+            $product->setTitle('Product'.rand(1,100));
+            $product->setDescription('smthh');
+            $product->setPrice(222);
+            $product->setQuantity(3);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
 }
